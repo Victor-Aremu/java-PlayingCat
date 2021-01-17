e8796becd42b056ba3fbcbdfa89cb2c761bbf05b
Victor-Aremu <77357316+Victor-Aremu@users.noreply.github.com>
2021-01-17 16:25:00 +0000

    small changes made

--- src/PlayingCat.java
+++ src/PlayingCat.java
@@ -1,10 +1,5 @@
 public class PlayingCat {
-    public static void main(String[] args) {
-        System.out.println(isCatPlaying(true, 10));
-        System.out.println(isCatPlaying(false, 36));
-        System.out.println(isCatPlaying(false, 35));
 
-    }
     public static boolean isCatPlaying(boolean summer, int temperature){
         if ((summer) && ((temperature > 36 && temperature <= 45))){
             return true;
@@ -13,9 +8,8 @@ public class PlayingCat {
             return true;
         }
         else {
-
             return false;
         }
     }
 }
-// Victor-Aremu
+// Victor.Aremu
