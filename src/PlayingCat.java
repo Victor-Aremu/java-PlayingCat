acb24bc3047bf41f96bb94764404b4e222771f5a
Victor-Aremu <77357316+Victor-Aremu@users.noreply.github.com>
2021-01-17 19:42:44 +0000

    value change

--- src/PlayingCat.java
+++ src/PlayingCat.java
@@ -1,7 +1,7 @@
 public class PlayingCat {
 
     public static boolean isCatPlaying(boolean summer, int temperature){
-        if ((summer) && ((temperature > 36 && temperature <= 45))){
+        if ((summer) && ((temperature > 25 && temperature <= 45))){
             return true;
         }
         else if((!summer) && ((temperature > 25 && temperature <= 35))){
